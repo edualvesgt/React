afb060021ca0e802f5a7dc9e399de77db6900889
Eduardo Alves Araújo Silva <46291995867@info.local>
2023-12-06 08:21:55 -0300

    Event continuacao

--- eventplus/src/pages/EventosAlunoPage/EventosAlunoPage.jsx
+++ eventplus/src/pages/EventosAlunoPage/EventosAlunoPage.jsx
@@ -8,6 +8,7 @@ import Container from "../../components/Container/Container";
 import { Select } from "../../components/FormComponents/FormComponents";
 import Spinner from "../../components/Spinner/Spinner";
 import Modal from "../../components/Modal/Modal";
+import Notification from '../../components/Notification/Notification';
 
 // Serviços
 import api, { eventsResource, MyEventsResource } from "../../Services/Service";
@@ -29,37 +30,54 @@ const EventosAlunoPage = () => {
   const [tipoEvento, setTipoEvento] = useState(""); // Código do tipo do Evento escolhido
   const [showSpinner, setShowSpinner] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  const [notifyUser, setNotifyUser] = useState({});
+
 
   // Recupera os dados globais do usuário
   const { userData, setUserData } = useContext(UserContext);
 
   // UseEffect para carregar os eventos baseado no tipo selecionado
   useEffect(() => {
+    // Função para carregar os tipos de eventos ao montar a página
+    async function loadEventsType() {
+
+      if (tipoEvento === "1") {
+        try {
+          const returnAllEvents = await api.get(eventsResource);
+
+          const returnEvents = await api.get(`${MyEventsResource}/${userData.userId}`);
+
+          const markedEvents = verifyPresence(returnAllEvents, returnEvents)
+          setEventos(markedEvents)
+
+          console.log("Todos eventos");
+          console.log(returnAllEvents.data);
+          console.log("MEus Eventos");
+          console.log(returnEvents.data);
+          console.log("eventos marcados");
+          console.log(markedEvents.data);
+
+        } catch (error) {
+          notifyError("Erro na API ")
+        }
+      } else if (tipoEvento === "2") {
+        try {
+          const returnEvents = await api.get(`${MyEventsResource}/${userData.userId}`);
+          // Verifique a estrutura dos dados retornados para acessar corretamente os eventos
+          const arrEvents = returnEvents.data.map((e) => e.evento);
+          setEventos(arrEvents);
+        } catch (error) {
+          notifyError("Erro na API")
+        }
+      } else {
+        setEventos([]);
+      }
+    }
+
     loadEventsType();
   }, [tipoEvento]);
 
-  // Função para carregar os tipos de eventos ao montar a página
-  async function loadEventsType() {
-    if (tipoEvento === "1") {
-      try {
-        const retorno = await api.get(eventsResource);
-        setEventos(retorno.data);
-      } catch (error) {
-        console.log(error);
-      }
-    } else if (tipoEvento === "2") {
-      try {
-        const returnEvents = await api.get(`${MyEventsResource}/${userData.userId}`);
-        // Verifique a estrutura dos dados retornados para acessar corretamente os eventos
-        const arrEventos = returnEvents.data.map((e) => e.evento);
-        setEventos(arrEventos);
-      } catch (error) {
-        console.log(error);
-      }
-    } else {
-      setEventos([]);
-    }
-  }
+
 
   // Função para verificar a presença de eventos do usuário em todos os eventos
   const verifyPresence = (arrAllEvents, eventsUser) => {
@@ -74,7 +92,8 @@ const EventosAlunoPage = () => {
         }
       }
     }
-
+    // Retorna todos os eventos Marcado com a presenca do Usuario
+    return arrAllEvents;
   }
 
   // Função para alternar entre "Meus Eventos" e "Todos os Eventos"
@@ -102,8 +121,40 @@ const EventosAlunoPage = () => {
     alert("Desenvolver a função conectar evento");
   }
 
+  const notifySuccess = (textNote) => {
+    setNotifyUser({
+        titleNote: "Sucesso",
+        textNote,
+        imgIcon: 'success',
+        imgAlt: 'Imagem de ilustração de sucesso. Moça segurando um balão com símbolo de confirmação ok.',
+        showMessage: true
+    });
+};
+
+const notifyError = (textNote) => {
+    setNotifyUser({
+        titleNote: "Erro",
+        textNote,
+        imgIcon: 'danger',
+        imgAlt: 'Imagem de ilustração de erro. Homem segurando um balão com símbolo de X.',
+        showMessage: true
+    });
+};
+
+const notifyWarning = (textNote) => {
+    setNotifyUser({
+        titleNote: "Aviso",
+        textNote,
+        imgIcon: 'warning',
+        imgAlt: 'Imagem de ilustração de aviso. Mulher em frente a um grande ponto de exclamação.',
+        showMessage: true
+    });
+};
+
+
   return (
     <>
+     {<Notification {...notifyUser} setNotifyUser={setNotifyUser} />}
       <MainContent>
         <Container>
           <Title titleText={"Eventos"} className="custom-title" />
